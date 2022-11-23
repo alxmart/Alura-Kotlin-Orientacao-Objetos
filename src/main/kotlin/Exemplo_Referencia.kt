04f9d673d9f4793ceb0af2aa75020952186c5fa8
alxmart <alxmartinez@gmail.com>
2022-11-23 16:28:51 -0300

    Adding more exercises.

---
+++ src/main/kotlin/Exemplo_Referencia.kt
@@ -0,0 +1,52 @@
+fun main() {
+
+    println("Bem vindo(a) ao ByteBank")
+
+    val contaxAlex = Contax()
+    contaxAlex.titular = "Alex"
+    contaxAlex.numero = 1000
+    contaxAlex.saldo = 200.0
+
+    val contaxFran = Contax()
+    contaxFran.titular = "Fran"
+    contaxFran.numero = 1001
+    contaxFran.saldo = 300.0
+
+//    println(contaFran.titular)
+//    println(contaFran.numero)
+//    println(contaFran.saldo)
+//
+//    println(contaAlex.titular)
+//    println(contaAlex.numero)
+//    println(contaAlex.saldo)
+
+    val numerox = 10
+    var numeroy = numerox
+    numeroy++
+
+    println("NumeroX: $numerox")
+    println("NumneroY: $numeroy")
+
+    val contaxJoao = Contax()
+    contaxJoao.titular = "João"
+
+    var contaMaria = contaxJoao
+    contaMaria.titular = "Maria"
+    // Alterando o mesmo objeto (contaJoao) Referência e não uma cópia !
+    contaxJoao.titular = "João"
+    // Alterou titular nos 2 objetos ( Joao => João / Maria => João )
+
+    println("Titular conta Joao: ${contaxJoao.titular}")
+    println("Titular conta Maria: ${contaMaria.titular}")
+
+    println(contaxJoao) //  Imprimiu: Conta@19dfb72a
+    println(contaMaria) //  Imprimiu: Conta@17c68925
+
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
---
+++ src/main/kotlin/Implementando_Getter_Setter.kt
@@ -0,0 +1,126 @@
+fun main() {
+
+    println("Bem vindo(a) ao ByteBank")
+
+    val contaAlex = Conta6()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.setSaldo(-200.0)
+
+    val contaFran = Conta6()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.setSaldo(300.0)
+
+    println("Titular: ${contaFran.titular}")
+    println("Número da Conta6: ${contaFran.numero}")
+    println("Saldo Conta6 Fran: ${contaFran.getSaldo()}")
+
+    println("Titular: ${contaAlex.titular}")
+    println("Número da Conta6: ${contaAlex.numero}")
+    println("Saldo Conta6 Alex: ${contaAlex.getSaldo()}")
+
+//    println("Depositando na conta do Alex")
+//    contaAlex.deposita(50.0)
+//    println("Saldo Conta6 Alex: ${contaAlex.saldo}")
+//
+//    println("Depositando na conta da Fran")
+//    contaFran.deposita(70.0)
+//    println("Saldo Conta6 Fran: ${contaFran.saldo}")
+//
+//    println("Sacando na conta do Alex")
+//    contaAlex.saca(250.0)
+//    println(contaAlex.saldo)
+//
+//    println("Sacando na conta da Fran")
+//    contaFran.saca(100.0)
+//    println(contaFran.saldo)
+//
+//    println("Saque em excesso na conta do Alex")
+//    contaAlex.saca(100.0)
+//    println("Saldo atual: ${contaAlex.saldo}")
+//
+//    println("Saque em excesso na conta da Fran")
+//    contaFran.saca(500.0)
+//    println("Saldo atual: ${contaFran.saldo}")
+//
+//    println("Transferência da conta da Fran para a conta do Alex")
+//    if(contaFran.transfere(800.0, contaAlex)) {
+//        println("Transferência efetivada")
+//    } else {
+//        println("Falha na transferência.")
+//    }
+//
+//    println("Saldo Conta6 do Alex: ${contaAlex.saldo}")
+//    println("Saldo Conta6 da Fran: ${contaFran.saldo}")
+
+}
+
+class Conta6 {
+    var titular = ""
+    var numero = 0
+    private var saldo = 0.0
+
+    // Métodos ou Comportamentos da classe:
+    fun deposita(valor: Double) {
+        this.saldo += valor
+    }
+
+    fun saca(valor: Double) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+        } else {
+            println("Saque não realizado - Valor Insuficiente")
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta6): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            //destino.saldo += valor
+            destino.deposita(valor)
+            return true
+        }
+        return false
+    }
+
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+    fun setSaldo(valor: Double) {
+        if (valor > 0) {
+            saldo = valor
+        }
+    }
+
+
+}
+
+//====================================================================
+
+fun testaCopiasEReferencia6() {
+
+    val numerox = 10
+    var numeroy = numerox
+    numeroy++
+
+    println("NumeroX: $numerox")
+    println("NumneroY: $numeroy")
+
+    val contaJoao = Conta6()
+    contaJoao.titular = "João"
+
+    var contaMaria = Conta6()
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("Titular conta Joao: ${contaJoao.titular}")
+    println("Titular conta Maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+
+}
+//====================================================================
+
