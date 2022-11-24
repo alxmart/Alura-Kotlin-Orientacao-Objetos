d8787c1164e3af5042526124822776331236d47b
alxmart <alxmartinez@gmail.com>
2022-11-24 15:21:16 -0300

    Adding more exercises.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,38 @@
 fun main() {
     println("Bem vindo(a) ao Bytebank.")
+    println()
 
-    testaLacos()
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+    println()
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+
 
 
 
 }
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+
 
