0c192c348383dccb9665de0e26c8f720de26c836
alxmart <alxmartinez@gmail.com>
2022-11-24 12:16:13 -0300

    Adding more exercises.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,38 +1,8 @@
 fun main() {
     println("Bem vindo(a) ao Bytebank.")
-    var i = 0
 
-    while (i < 5) {
+    testaLacos()
 
-        val titular: String = "Alex $i"
-        val numeroConta: Int = 1000 + i
-        var saldo: Double = i + 10.0
-
-        println("Titular: $titular")
-        println("Número da Conta: $numeroConta")
-        println("Saldo da Conta: $saldo")
-        println()
-
-        i++
-
-    }
-
-//    for (i in 5 downTo 1 /*step 2*/ ) {
-//
-//        if (i == 4) {
-//            /*break*/ continue
-//        }
-//
-//        val titular: String = "Alex $i"
-//        val numeroConta: Int = 1000 + i
-//        var saldo: Double = i + 10.0
-//
-//        println("Titular: $titular")
-//        println("Número da Conta: $numeroConta")
-//        println("Saldo da Conta: $saldo")
-//        println()
-
-    // testaCondicoes(saldo)
 
 
 }
